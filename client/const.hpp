22c0da3434137a02ed264bfa3797271a9b796d19
Willi Schinmeyer <willi@schinmeyer.de>
2013-08-25 23:36:41 +0200

    Client code stubs/refactoring. Using more constants.

---
+++ client/const.hpp
@@ -0,0 +1,9 @@
+#ifndef CLIENT_CONST_HPP_INCLUDED
+#define CLIENT_CONST_HPP_INCLUDED
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#define WINDOW_TITLE "MultiPaint"
+
+#endif
--- client/main.cpp
+++ client/main.cpp
@@ -1,18 +1,63 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+
+#include <string>
+#include <iostream>
+
+#include "../common/const.hpp"
+#include "const.hpp"
+
+enum GameState
+{
+  GSAwaitingTurn,
+  GSDrawing
+};
+
+bool setup(int argc, char** argv, sf::TcpSocket& out_socket, std::string& out_error)
+{
+  // TODO
+  return true;
+}
+
+void handleWindowEvents(sf::Window& wnd, const GameState gameState)
+{
+  sf::Event ev;
+  while(wnd.pollEvent(ev))
+  {
+    if(ev.type == sf::Event::Closed)
+    {
+      wnd.close();
+    }
+  }
+}
 
 int main(int argc, char** argv)
 {
-  sf::RenderWindow wnd(sf::VideoMode(480, 320), "MultiPaint", sf::Style::Close, sf::ContextSettings(0, 0, 0, 2, 0));
+  // Connect to server
+  sf::TcpSocket socket;
+  {
+    std::string error;
+    if(!setup(argc, argv, socket, error))
+    {
+      std::cout << error << std::endl;
+      return 0;
+    }
+  }
+
+  // Create Window
+  sf::RenderWindow wnd(
+    sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+    WINDOW_TITLE,
+    sf::Style::Close,
+    sf::ContextSettings(0, 0, 0, 2, 0)
+    );
+
+  GameState gameState = GSAwaitingTurn;
+
+  // Main loop
   while(wnd.isOpen())
   {
-    sf::Event ev;
-    while(wnd.pollEvent(ev))
-    {
-      if(ev.type == sf::Event::Closed)
-      {
-        wnd.close();
-      }
-    }
+    handleWindowEvents(wnd, gameState);
 
     wnd.clear();
     wnd.display();
